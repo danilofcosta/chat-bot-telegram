5f0caf6874a10cf64aab0be8458a7cb2d4395241
Danilo Costa <117859128+danilofcosta@users.noreply.github.com>
2022-11-24 11:02:10 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+from tokem import tokem as chavet
+from defs import*
+import telebot
+bot = telebot.TeleBot(chavet)
+temp=[]
+memory=load()
+#verifica se chegou mensagem
+def verificar(mensagem):
+    print(memory)
+    print(mensagem.chat.first_name)
+    print(mensagem.text)
+    return True
+@bot.message_handler(commands=["start"])
+def start(mensagem):
+    bot.send_message(mensagem.chat.id,f'{mensagem.chat.first_name} Dizem que não e bom invadir pv sem pedir mais enfim......\n tudo bem?')
+
+@bot.message_handler(func=verificar)
+def responder(mensagem):
+    msguser=mensagem.text
+    memory=load()
+    if  'qual meu nome?'in msguser.lower() :
+        bot.reply_to(mensagem,f' Seu nome {mensagem.chat.first_name}')
+        bot.send_message(mensagem.chat.id,'esqueceu?')
+
+    elif msguser in memory:
+        bot.reply_to(mensagem,memory[mensagem.text])
+    else:
+        if len(temp) == 0:
+            temp.append(msguser)
+            print(temp)
+            bot.reply_to(mensagem,f'o que devo responder da proxima vez que me mandarem \n > {temp[0]} <')
+        else:
+            autosave=msguser
+            database_joson(temp[0],autosave)
+            bot.send_message(mensagem.chat.id,f'da proxima vez irei responder isso \n {temp[0],autosave}')
+            temp.clear()
+            print(temp)
+#vericação de updates continua
+bot.polling()
