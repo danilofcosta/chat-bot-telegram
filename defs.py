ad9d0d13d28735c30622ccbd2eddc67f413926f8
Danilo Costa <117859128+danilofcosta@users.noreply.github.com>
2022-11-29 14:37:20 -0300

    Update defs.py
    adicição de nova chave

--- defs.py
+++ defs.py
@@ -3,7 +3,7 @@ def create_database (nome_aquivo):
     import os,json
     #criada aquivo .json
     loading=os.getcwd()
-    start={'messafem_txt':{'oi':'ola'}}
+    start={'messagem_txt':{'oi':'ola'},"invazao_de_pv": {'NOME':'id'},"sticker":{"id unico":'file id'}}
     for file in os.listdir(loading):
         if file == f'{nome_aquivo}.json':
             return f'{nome_aquivo}.json'
@@ -16,11 +16,11 @@ def create_database (nome_aquivo):
         return f'{nome_aquivo}.json'
      
 
-def database_joson(chave,valor,namekye='messafem_txt',database='database'):
+def database_joson(chave='',valor=0,namekye='messagem_txt',database='database'):
     import json
     #criada aquivo .json
     with open(f'{database}.json','r') as  databaseLoad:
-        aquivoload=json.load(databaseLoad)#carrega o que esta salvo na base de dados ecoloca em uma variavel /dicionario
+        aquivoload=json.load(databaseLoad)#carrega o que esta salvo na base de dados ecoloca em uma variavel /dicionario         
         aquivoload[namekye][chave]=valor
         with open(f'{database}.json','w') as  databasesave:
             json.dump(aquivoload,databasesave,indent=4)
